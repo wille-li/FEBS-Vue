b8a032c26a9b9507c049362f6137d00c777a57a1
a5723942 <Administrator@SC-201908132150>
2019-09-19 20:27:11 +0800

    我的消息api

--- backend/src/main/java/cc/mrbird/febs/api/controller/MessageController.java
+++ backend/src/main/java/cc/mrbird/febs/api/controller/MessageController.java
@@ -1,16 +1,12 @@
 package cc.mrbird.febs.api.controller;
 
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import cc.mrbird.febs.api.domain.Message;
 import cc.mrbird.febs.api.domain.Result;
 import cc.mrbird.febs.api.service.MessageService;
 import cc.mrbird.febs.api.util.ResultUtil;
@@ -33,19 +29,5 @@ public class MessageController extends BaseController{
     @ApiOperation("我的消息")
     public Result selectMyMessage(@PathVariable Integer userId) throws FebsException {
         return ResultUtil.success(messageService.selectMessageList(userId));
-    }
-    
-    @Log("所有人的消息")
-    @GetMapping("/selectAllMessage")
-    @ApiOperation("所有的人消息")
-    public Result selectAllMessage() throws FebsException {
-        return ResultUtil.success(messageService.selectMessageList(null));
-    }
-    
-    @Log("新增消息")
-    @PostMapping("addMessage")
-    @ApiOperation("新增消息")
-    public Result addMessage(@Valid Message message) throws FebsException {
-        return ResultUtil.success(messageService.addMessage(message));
     }
 }
